80fceb69b918902493fc69b483ca740b1187154d
ScaerieTale <7010800+ScaerieTale@users.noreply.github.com>
2023-05-14 22:28:12 -0500

    Add code to stop audio sting and crash VFX from playing multiple times

--- Assets/Scripts/CrashDetector.cs
+++ Assets/Scripts/CrashDetector.cs
@@ -11,17 +11,18 @@ public class CrashDetector : MonoBehaviour
     // set linear drag after player crashes
     [SerializeField] float linearDrag = 10;
     [SerializeField] AudioClip crashSFX;
-
+    private bool hasCrashed = false;
 
 
     private void OnTriggerEnter2D(Collider2D other) {
-        if (other.tag == "Ground") {
+        if (other.tag == "Ground" && !hasCrashed) {
+            hasCrashed = true;
+            
             FindObjectOfType<PlayerController>().DisableControls();
             crashEffect.Play();
             GetComponent<AudioSource>().PlayOneShot(crashSFX);
             ground.GetComponent<SurfaceEffector2D>().enabled = false;
             GetComponent<Rigidbody2D>().drag = linearDrag;
-            
             // Invoke the ReloadScene() method below and give
             // it a length of time set in invokeDelay above.
             Invoke("ReloadScene", invokeDelay);
