5fc7a1219040c18beb5bc17e9cec9acbbacd156a
ScaerieTale <7010800+ScaerieTale@users.noreply.github.com>
2023-05-18 04:23:44 -0500

    Experimenting with various UI elements and audio
    No major build changes yet

--- Assets/Scripts/PlayerDustTrail.cs
+++ Assets/Scripts/PlayerDustTrail.cs
@@ -4,22 +4,22 @@ using UnityEngine;
 
 public class PlayerDustTrail : MonoBehaviour
 {
-[SerializeField] ParticleSystem dustTrail;
+    [SerializeField] ParticleSystem dustTrail;
+    float isOnGround;
 
-private void OnCollisionEnter2D(Collision2D other)
-{
-    if (other.gameObject.tag == "Ground")
+    private void OnCollisionEnter2D(Collision2D other)
     {
-        dustTrail.Play();
+        if (other.gameObject.tag == "Ground")
+        {
+            dustTrail.Play();
+        }
     }
-}
 
-private void OnCollisionExit2D(Collision2D other)
-{
-    if (other.gameObject.tag == "Ground")
+    private void OnCollisionExit2D(Collision2D other)
     {
-        dustTrail.Stop();
-    }
-}
-    
+        if (other.gameObject.tag == "Ground")
+        {
+            dustTrail.Stop();
+        }
+    }   
 }
