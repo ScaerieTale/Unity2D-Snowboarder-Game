48e6d44db1e235a00b9dbe5694b8c03e96df2f0f
ScaerieTale <7010800+ScaerieTale@users.noreply.github.com>
2023-05-18 20:28:00 -0500

    Hotfix for crash particle FX not playing when player crashes

--- Assets/Scripts/PlayerAVFX.cs
+++ Assets/Scripts/PlayerAVFX.cs
@@ -4,15 +4,5 @@ using UnityEngine;
 
 public class PlayerAVFX : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
 }
