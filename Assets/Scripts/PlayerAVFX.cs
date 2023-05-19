ebc1fd0304216d7d65d051a88376f344afb38f5c
ScaerieTale <7010800+ScaerieTale@users.noreply.github.com>
2023-05-18 21:12:52 -0500

    Set up two methods, PlayFX and StopFX that can be called to play or stop both SFX and VFX

--- Assets/Scripts/PlayerAVFX.cs
+++ Assets/Scripts/PlayerAVFX.cs
@@ -4,5 +4,32 @@ using UnityEngine;
 
 public class PlayerAVFX : MonoBehaviour
 {
-    
+    [SerializeField] ParticleSystem dustTrail;
+    [SerializeField] AudioClip playerLandSFX;
+    [SerializeField] AudioClip snowboardSFX;
+    private AudioSource audioPlayer;
+    private Rigidbody2D rb2d;
+
+    private void Start()
+    {
+        audioPlayer = GetComponent<AudioSource>();
+        
+    }
+    void OnCollisionEnter2D(Collider2D other)
+    {
+
+    }
+
+    private void PlayFX()
+    {
+        dustTrail.Play();
+        audioPlayer.PlayOneShot(playerLandSFX);
+        audioPlayer.Play();
+    }
+    private void StopFX()
+    {
+        dustTrail.Stop();
+        audioPlayer.Stop();
+    }
+
 }
